39504c71309c8eef1174b792aec0b5f9df04ff2c
wevertoncouy <wevertoncouy@gmail.com>
2017-08-23 22:33:04 -0300

    Primeira versão

---
+++ WebAppAula02_lab2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAula02_lab2.Startup))]
+namespace WebAppAula02_lab2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
